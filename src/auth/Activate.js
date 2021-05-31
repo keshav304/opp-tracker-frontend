2cde0a65f1ce82e3ca1c7eabc3c2499385b9946d
keshav304 <keshavjhaa2678@gmail.com>
2021-05-31 20:22:18 +0530

    debug account activation error

--- src/auth/Activate.js
+++ src/auth/Activate.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect,useCallback } from "react";
 import { Link } from "react-router-dom";
 import axios from "axios";
 import { ToastContainer, toast } from "react-toastify";
@@ -16,14 +16,17 @@ const Activate = ({ match }) => {
     token: "",
     show: true,
   });
-  useEffect(() => {
+  const handleAcctivation = useCallback(() => {
     const token = match.params.token;
     let { name } = jwt.decode(token);
     if (token) {
       setValues({ ...values, name, token });
     }
-  
-  }, [match.params.token, values]);
+  },[match.params.token, values])
+ 
+  useEffect(() => {
+    handleAcctivation()
+  }, [handleAcctivation]);
 
   const { name, token } = values;
 
